68e3971ecc999e95d1e900ee7f3e812766f4c85b
Kevin Phan <Kevin-P-Phan@users.noreply.github.com>
2022-12-02 21:58:01 -0500

    Part 2 of day 2

--- advent_of_code_day2/src/main.rs
+++ advent_of_code_day2/src/main.rs
@@ -6,7 +6,8 @@ fn main() {
     // File hosts must exist in current path before this produces output
     match read_lines("src/input.txt") {
         Ok(lines) => {
-            let mut points: i32 = 0;
+            let mut part1_points: i32 = 0;
+            let mut part2_points: i32 = 0;
             // Consumes the iterator, returns an (Optional) String
             for line in lines {
                 if let Ok(game) = line {
@@ -18,23 +19,39 @@ fn main() {
                         || ("B" == result[0] && result[1] == "Z")
                         || ("C" == result[0] && result[1] == "X")
                     {
-                        points += 6;
+                        part1_points += 6;
                     } else if ("A" == result[0] && result[1] == "X")
                         || ("B" == result[0] && result[1] == "Y")
                         || ("C" == result[0] && result[1] == "Z")
                     {
-                        points += 3;
+                        part1_points += 3;
                     }
-                    if result[1] == "X" {
-                        points += 1;
-                    } else if result[1] == "Y" {
-                        points += 2;
+                    points_from_shape(&result, &mut part1_points);
+                    if result[1] == "Y" {
+                        part2_points += 3;
+                        points_from_opp_shape(&result, &mut part2_points);
+                    } else if result[1] == "Z" {
+                        part2_points += 6;
+                        if result[0] == "A" {
+                            part2_points += 2;
+                        } else if result[0] == "B" {
+                            part2_points += 3;
+                        } else {
+                            part2_points += 1;
+                        }
                     } else {
-                        points += 3;
+                        if result[0] == "A" {
+                            part2_points += 3;
+                        } else if result[0] == "B" {
+                            part2_points += 1;
+                        } else {
+                            part2_points += 2;
+                        }
                     }
                 }
             }
-            println!("The sum of the points is {}.", points)
+            println!("The sum of the points for part 1 is {}.", part1_points);
+            println!("The sum of the points for part 2 is {}.", part2_points);
         }
         Err(_) => println!("You did not supply the correct input file."),
     }
@@ -46,4 +63,24 @@ where
 {
     let file: File = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn points_from_shape(result: &Vec<&str>, points: &mut i32) {
+    if result[1] == "X" {
+        *points += 1;
+    } else if result[1] == "Y" {
+        *points += 2;
+    } else {
+        *points += 3;
+    }
+}
+
+fn points_from_opp_shape(result: &Vec<&str>, points: &mut i32) {
+    if result[0] == "A" {
+        *points += 1;
+    } else if result[0] == "B" {
+        *points += 2;
+    } else {
+        *points += 3;
+    }
 }
