e7792f6c641d8bf280a0c8a1325942d60f4b9674
Kevin Phan <Kevin-P-Phan@users.noreply.github.com>
2022-12-04 19:16:15 -0500

    Part 2 of day 3

--- advent_of_code_day3/src/main.rs
+++ advent_of_code_day3/src/main.rs
@@ -5,21 +5,30 @@ use std::path::Path;
 
 fn main() {
     let mut sum = 0;
+    let mut sum_part2 = 0;
+    let mut line_counter = 0;
+    let mut items_vec: VecDeque<String> = VecDeque::new();
     // File hosts must exist in current path before this produces output
     match read_lines("src/input.txt") {
         Ok(lines) => {
             // Consumes the iterator, returns an (Optional) String
             for line in lines {
                 if let Ok(item) = line {
+                    items_vec.push_front(item.clone());
                     let mid: usize = item.len() / 2;
                     let items: (&str, &str) = item.split_at(mid);
                     sum += check_items(items.0, items.1);
+                    line_counter += 1;
+                    if line_counter % 3 == 0 {
+                        sum_part2 += check_items_vecs(&items_vec[0], &items_vec[1], &items_vec[2]);
+                    }
                 }
             }
         }
         Err(_) => println!("You did not supply the correct input file."),
     }
     println!("The sum of the priorties is {}", sum);
+    println!("The sum of the group priorties is {}", sum_part2);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -30,7 +39,7 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn check_items(item1: &str, item2: &str) -> i32 {
+fn priorities_map_generator() -> HashMap<char, i32> {
     let mut upper_alphabet: VecDeque<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect();
     let mut lower_alphabet: VecDeque<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
     let mut priorities: HashMap<char, i32> = HashMap::new();
@@ -42,7 +51,11 @@ fn check_items(item1: &str, item2: &str) -> i32 {
         let letter = lower_alphabet.pop_front().unwrap();
         priorities.insert(letter, i);
     }
-    println!("{}", priorities.get(&'t').unwrap());
+    return priorities;
+}
+
+fn check_items(item1: &str, item2: &str) -> i32 {
+    let priorities: HashMap<char, i32> = priorities_map_generator();
     let mut priority: i32 = 0;
     let mut item1_chars: HashSet<char> = HashSet::new();
     let mut item2_chars: HashSet<char> = HashSet::new();
@@ -55,7 +68,30 @@ fn check_items(item1: &str, item2: &str) -> i32 {
     for i in item1_chars.into_iter() {
         if item2_chars.contains(&i) {
             priority = *priorities.get(&i).unwrap();
-            println!("{} has value {}", &i, priority)
+        }
+    }
+
+    return priority;
+}
+
+fn check_items_vecs(item1: &str, item2: &str, item3: &str) -> i32 {
+    let priorities: HashMap<char, i32> = priorities_map_generator();
+    let mut priority: i32 = 0;
+    let mut item1_chars: HashSet<char> = HashSet::new();
+    let mut item2_chars: HashSet<char> = HashSet::new();
+    let mut item3_chars: HashSet<char> = HashSet::new();
+    for i in item1.chars() {
+        item1_chars.insert(i);
+    }
+    for i in item2.chars() {
+        item2_chars.insert(i);
+    }
+    for i in item3.chars() {
+        item3_chars.insert(i);
+    }
+    for i in item1_chars.into_iter() {
+        if item2_chars.contains(&i) && item3_chars.contains(&i) {
+            priority = *priorities.get(&i).unwrap();
         }
     }
 
